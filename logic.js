24bcca87936855b1bf5de6a2d84a5e6f9683d52b
Eric Scott <scottzillaster@gmail.com>
2019-12-18 14:56:07 -0600

    modularized the ajax call a little- use handleMovieInfo as the function that handles the ajax response

--- logic.js
+++ logic.js
@@ -1,10 +1,16 @@
 var movie = prompt("What movie do you want to watch");
-var url = "https://www.omdbapi.com/?t=" + movie.replace(/ /g, "+") + "&apikey=trilogy";
-$.ajax({
-    url: url,
-    method: "GET"
-}).then(function(response){
+
+function handleMovieInfo(response){
     console.log(response);
     console.log(response.Poster);
     console.log(response.Title);
-})
+}
+function getMovieInfo(movie){
+    var url = "https://www.omdbapi.com/?t=" + movie.replace(/ /g, "+") + "&apikey=trilogy";
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(handleMovieInfo);
+}
+
+getMovieInfo(movie);
